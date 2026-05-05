79e74c2e34818a69025c3f6b516d6e2631055ef0
Akitaroshi <mafanasev292@gmail.com>
2026-05-05 14:40:24 +0300

    Added UI with Tailwind CSS

--- library/views.py
+++ library/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all().select_related('author', 'genre')
+    return render(request, 'library/book_list.html', {'books': books})
