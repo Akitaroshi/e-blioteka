f445d63b84a50c8ebe0cb45a8a28fe676a74f98e
Akitaroshi <mafanasev292@gmail.com>
2026-05-07 14:54:26 +0300

    fixes&features

--- library/views.py
+++ library/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render, get_object_or_404
-from library.models import Book
+from django.shortcuts import render, get_object_or_404  # <--- ПРОВЕРЬ ТУТ
+from django.core.paginator import Paginator
+from .models import Book, Genre
 
 def book_list(request):
-    books = Book.objects.all().prefetch_related('authors').select_related('genre')
-    return render(request, 'library/book_list.html', {'books': books})
+    books_queryset = Book.objects.all().prefetch_related('authors').select_related('genre')
+    genres = Genre.objects.all()
+
+    search_query = request.GET.get('search', '')
+    if search_query:
+        books_queryset = books_queryset.filter(title__icontains=search_query)
+
+    genre_id = request.GET.get('genre', '')
+    if genre_id:
+        books_queryset = books_queryset.filter(genre_id=genre_id)
+
+    paginator = Paginator(books_queryset, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'books': page_obj,
+        'genres': genres,
+        'search_query': search_query,
+        'selected_genre': genre_id,
+    }
+    return render(request, 'library/book_list.html', context)
 
 def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
